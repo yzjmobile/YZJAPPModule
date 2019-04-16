e73abd7dbe9a1d1b48e061a3a97423f570feda5f
quding <ding_qu@kingdee.com>
2019-04-16 16:27:05 +0800

    fix

--- Assets/YZJAPPModule.framework/Headers/YZJAppModuleManager.h
+++ Assets/YZJAPPModule.framework/Headers/YZJAppModuleManager.h
@@ -26,6 +26,47 @@ typedef NS_ENUM(NSInteger, YZJLanguageType) {
 
 - (void)languageChanged:(YZJLanguageType)type;
 
+/// system callback
+//- (void)applicationDidFinishLaunch:(UIApplication *)application;
+//- (void)applicationWillResignActive:(UIApplication *)application;
+//- (void)applicationDidEnterBackground:(UIApplication *)application;
+//- (void)applicationWillEnterForeground:(UIApplication *)application;
+//- (void)applicationDidBecomeActive:(UIApplication *)application;
+//- (void)applicationWillTerminate:(UIApplication *)application;
+
+
+/// router 处理
+- (NSArray<NSString *> *)supportUrl;
+- (void)executeRouterUrl:(NSString *)url params:(NSDictionary *)dict vc:(UIViewController *)vc;
+
+/// root vc
+- (nonnull NSArray<NSString *> *)rootIdentifiers;
+- (UIViewController *)rootVCWithIdentifier:(nonnull NSString *)identifier;
+/**
+ 使用示例：
+ 通过这种方式 一个 module 可以设置多个 tabar 的 vc
+ - (nonnull NSArray<NSString *> *)rootIdentifiers {
+     return @[
+             @"mail",
+             @"workbench"
+            ];
+ }
+
+ - (UIViewController *)rootVCWithIdentifier:(nonnull NSString *)identifier {
+     if ([identifier isEqualToString:@"mail"]) {
+        return [[UIViewController alloc] init];
+     } else if ([identifier isEqualToString:@"workbench"]) {
+        return [[UIViewController alloc] init];
+     }
+ 
+     return nil;
+ }
+ 
+ 
+ */
+
+
+
 @end
 
 @interface YZJAppModuleManager : NSObject
